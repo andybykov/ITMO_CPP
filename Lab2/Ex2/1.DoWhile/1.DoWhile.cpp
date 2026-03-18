1561289e093288417c2ee856d095ce4f4096e408
Andrey Bykov <portperm@yandex.ru>
2026-03-19 01:02:48 +0300

    aControl Exercise.1 SuperprimeNumber

---
+++ Lab2/Ex2/1.DoWhile/1.DoWhile.cpp
@@ -0,0 +1,28 @@
+﻿/* Использование цикла с постусловием
+* В этом задании вы используете цикл с постусловием для вывода значения функции на интервале.
+*/
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	double x, x1, x2, y; // x – аргумент функции, x1, x2 – границы интервала, y – выходной параметр
+	cout << "x1 = "; cin >> x1;
+	cout << "x2 = "; cin >> x2;
+	cout << "\tx\tsin(x)\n";
+	x = x1;
+	do
+	{
+		y = sin(x);
+		cout << "\t" << x << "\t" << y << endl;
+		x = x + 0.01;
+	} while (x <= x2);
+	return 0;
+}
+
+
+
+
