9346937d618264325fbc1bfbeed7723e07db644f
Andrey Bykov <portperm@yandex.ru>
2026-03-18 17:41:10 +0300

    add Lab2/Exercise 2

---
+++ Lab2/Ex2.SwitchCase/Ex2.SwitchCase.cpp
@@ -0,0 +1,28 @@
+﻿/* Использование оператора switch при реализации выбора */
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    char op;
+    std::cout << "Сделай свой выбор, собери авто свой мечты: ";
+    std::cin >> op;
+
+       switch (op)
+    {
+    case 'S':
+        std::cout << "Радио играть должно\n";
+        std::cout << "Колеса круглые\n";
+        std::cout << "Мощный двигатель\n";
+        break;
+    case 'V':
+        std::cout << "Кондиционер хочу\n";
+        std::cout << "Радио играть должно\n";
+        std::cout << "Колеса круглые\n";
+        std::cout << "Мощный двигатель\n";
+        break;
+    default:
+        std::cout << "Колеса круглые\n";
+        std::cout << "Мощный двигатель\n";
+    }
+}
