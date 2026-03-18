2849c5c3ca4718d2c2a9e9a817f53d80634a70dc
Andrey Bykov <portperm@yandex.ru>
2026-03-18 17:17:07 +0300

    add Lab2/Exercise 1

---
+++ Lab2/Ex1.IfElse/Ex1.IfElse.cpp
@@ -0,0 +1,30 @@
+﻿/* В этом задании должна быть проверка лежит ли точка внутри заштрихованной области, или она
+* находится вне заштрихованной области или на ее границе
+*/
+#include <iostream>
+
+int main()
+{
+	int x{ 0 };
+	int y{ 0 };
+
+	std::cout << "Enter coordinats: x y" << std::endl;
+
+	std::cin >> x >> y;
+	
+		if (x * x + y * y < 9 && y > 0) {
+			// "внутри"
+			std::cout << "Inside area" << std::endl;
+		}
+
+		else if (x * x + y * y > 9 || y < 0) {
+			// "снаружи"
+			std::cout << "Outside area" << std::endl;
+		}
+
+		else {
+			// "на границе"
+			std::cout << "On border of area" << std::endl;
+		}
+
+}
